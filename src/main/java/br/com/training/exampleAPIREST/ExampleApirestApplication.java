35cc23690a857945f886eb697789a715d3c682b7
Daniel Diniz <daniel.diniz1@icloud.com>
2023-03-28 19:46:06 -0300

    first commit

---
+++ src/main/java/br/com/training/exampleAPIREST/ExampleApirestApplication.java
@@ -0,0 +1,13 @@
+package br.com.training.exampleAPIREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApirestApplication.class, args);
+	}
+
+}
