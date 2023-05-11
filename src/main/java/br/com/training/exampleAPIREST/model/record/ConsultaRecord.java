134bcf0222bcaadda035244638067202896b09cd
Daniel Diniz <daniel.diniz1@icloud.com>
2023-05-10 21:29:02 -0300

    inclusão de jsonalias para tratar recebimento de dados no record

--- src/main/java/br/com/training/exampleAPIREST/model/record/ConsultaRecord.java
+++ src/main/java/br/com/training/exampleAPIREST/model/record/ConsultaRecord.java
@@ -1,5 +1,7 @@
 package br.com.training.exampleAPIREST.model.record;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 
@@ -7,5 +9,8 @@ import java.time.LocalDateTime;
 
 public record ConsultaRecord(String idMedico,
                              @NotNull String idPaciente,
-                             @NotNull @Future LocalDateTime data) {
+                             @NotNull
+                             @Future
+                             @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+                             @JsonAlias({"data_consulta","horario"}) LocalDateTime data) {
 }
